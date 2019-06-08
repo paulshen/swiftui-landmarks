4fd445ddf271aaebcbf0c4edc0188ec7065ae045
paulshen <paulshen0@gmail.com>
2019-06-08 11:41:48 -0700

    hstack padding

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,19 @@ import SwiftUI
 
 struct ContentView : View {
   var body: some View {
-    VStack {
+    VStack(alignment: .leading) {
       Text("Turtle Rock")
         .font(.title)
         .color(.black)
-      Text("Joshua Tree National Park")
+      HStack {
+        Text("Joshua Tree National Park")
+          .font(.subheadline)
+        Spacer()
+        Text("California")
+          .font(.subheadline)
+      }
     }
+    .padding()
   }
 }
 
