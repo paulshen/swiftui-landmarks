197b10081b7469757d033e8d707722b611c864a9
paulshen <paulshen0@gmail.com>
2019-06-08 11:56:51 -0700

    compose detail view

--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -15,7 +15,8 @@ struct MapView : UIViewRepresentable {
   }
   
   func updateUIView(_ view: MKMapView, context: Context) {
-    let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.16868)
+    let coordinate = CLLocationCoordinate2D(
+      latitude: 34.011286, longitude: -116.166868)
     let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
     let region = MKCoordinateRegion(center: coordinate, span: span)
     view.setRegion(region, animated: true)
